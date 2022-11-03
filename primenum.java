a95b3f0e83fe79af9cee164540a296ee345bec54
Moin Pasha <116783467+moinmox@users.noreply.github.com>
2022-11-03 12:58:56 +0530

    Add files via upload

---
+++ primenum.java
@@ -0,0 +1,34 @@
+package javaexamples;
+import java.util.*;
+public class primenum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int n, i, flag = 0;
+		  System.out.println("Enter a positive integer: ");
+		  n=s.nextInt();
+
+		  // 0 and 1 are not prime numbers
+		  // change flag to 1 for non-prime number
+		  if (n == 0 || n == 1)
+		    flag = 1;
+
+		  for (i = 2; i <= n / 2; i++) {
+
+		    // if n is divisible by i, then n is not prime
+		    // change flag to 1 for non-prime number
+		    if (n % i == 0) {
+		      flag = 1;
+		      break;
+		    }
+		  }
+
+		  // flag is 0 for prime numbers
+		  if (flag == 0)
+			  System.out.println(n+" is a prime number.");
+		  else
+			  System.out.println(n+" is not a prime number.");
+	}
+
+}
